a60122e2c2970a62a5681b820fba9394176dc8da
Fandorin1000 <47949114+Fandorin1000@users.noreply.github.com>
2019-09-25 17:26:15 +0300

    0014 задание

---
+++ 0013 ввод двух чисел = их сумма (prompt)/script.js
@@ -0,0 +1,12 @@
+/*
+
+Создайте страницу, которая предлагает ввести два числа и выводит их сумму.
+
+*/
+
+let numberOne = +prompt('enter numberOne', '');
+let numberTwo = +prompt('enter numberTwo', '');
+
+let result = (numberOne + numberTwo);
+
+alert(`Сумма дву чисел: ${result}`);
