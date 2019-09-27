f597cad6870c8a96cd050c52bf6387793ba390b8
Fandorin1000 <47949114+Fandorin1000@users.noreply.github.com>
2019-09-27 14:19:17 +0300

    0031

---
+++ 0031 сумма всех prompt while(true)/script.js
@@ -0,0 +1,19 @@
+function sumPrompt () {
+	let numbers = []
+
+	while(true) {
+
+		let enterNumber = prompt("введите число", "");
+
+		if(enterNumber === "" || enterNumber === null || !isFinite(enterNumber) ) break;
+
+		numbers.push(+enterNumber);
+	}
+
+	let sum = 0;
+	for(let number of numbers) {
+		sum +=number;
+	}
+	return sum;
+}
+alert(sumPrompt() );
