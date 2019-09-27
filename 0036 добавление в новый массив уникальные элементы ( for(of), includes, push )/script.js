c580d2f7c0f6b7a78c33ddf0f86012e15a91cca6
Fandorin1000 <47949114+Fandorin1000@users.noreply.github.com>
2019-09-27 14:20:43 +0300

    0036

---
+++ 0036 добавление в новый массив уникальные элементы ( for(of), includes, push )/script.js
@@ -0,0 +1,16 @@
+function unique(arr) {
+  let result = [];
+
+  for(let item of arr) {
+  	if (!result.includes(item)) {
+  		result.push(item);
+  	}
+  }
+  return result;
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+alert( unique(strings) ); // кришна, харе, :-O
