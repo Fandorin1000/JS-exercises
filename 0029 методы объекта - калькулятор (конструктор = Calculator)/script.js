13169f0d65b7f554197b4b3e2c7e9659c6e37f50
Fandorin1000 <47949114+Fandorin1000@users.noreply.github.com>
2019-09-27 14:18:32 +0300

    0029

---
+++ 0029 методы объекта - калькулятор (конструктор = Calculator)/script.js
@@ -0,0 +1,38 @@
+/*
+
+Создать Calculator при помощи конструктора
+важность: 5
+Напишите функцию-конструктор Calculator, которая создает объект с тремя методами:
+
+Метод read() запрашивает два значения при помощи prompt и запоминает их в свойствах объекта.
+Метод sum() возвращает сумму запомненных свойств.
+Метод mul() возвращает произведение запомненных свойств.
+Пример использования:
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Сумма=" + calculator.sum() );
+alert( "Произведение=" + calculator.mul() );
+
+*/
+
+function Calculator() {
+
+	this.read = function() {
+		this.a = +prompt('a?', ' ');
+		this.b = +prompt('b?', ' ');
+	};
+	this.sum = function() {
+		return this.a + this.b;
+	};
+	this.mul = function() {
+		return this.a * this.b;
+	};
+}
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Сумма=" + calculator.sum() );
+alert( "Произведение=" + calculator.mul() );
+
