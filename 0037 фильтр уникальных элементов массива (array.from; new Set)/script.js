8401d36675f31fe0a978af5ffb510df1228ce1fd
Fandorin1000 <47949114+Fandorin1000@users.noreply.github.com>
2019-09-27 14:21:00 +0300

    0037

---
+++ 0037 фильтр уникальных элементов массива (array.from; new Set)/script.js
@@ -0,0 +1,12 @@
+//Создайте функцию unique(arr),
+//которая вернёт массив уникальных, не повторяющихся значений массива arr.
+
+function unique(arr) {
+    return Array.from(new Set(arr));
+  }
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert( unique(values) ); // Hare, Krishna, :-O
