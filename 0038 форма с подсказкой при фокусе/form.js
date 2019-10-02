60f81df025494664f176bdf79486b4be4e20cd23
Fandorin1000 <47949114+Fandorin1000@users.noreply.github.com>
2019-10-02 17:06:15 +0300

    форма с подсказкой при фокусе

---
+++ 0038 форма с подсказкой при фокусе/form.js
@@ -0,0 +1,24 @@
+function showHelp(help) { 
+  document.getElementById('help').innerHTML = help;
+}
+
+function makeHelpCallback(help) {
+  return function() {
+    showHelp(help);
+  };
+}
+
+function setupHelp() {
+  var helpText = [
+      {'id': 'email', 'help': 'Ваш адрес e-mail'},
+      {'id': 'name', 'help': 'Ваше полное имя'},
+      {'id': 'age', 'help': 'Ваш возраст (Вам должно быть больше 16)'}
+    ];
+
+  for (var i = 0; i < helpText.length; i++) {
+    var item = helpText[i];
+    document.getElementById(item.id).onfocus = makeHelpCallback(item.help);
+  }
+}
+
+setupHelp();
